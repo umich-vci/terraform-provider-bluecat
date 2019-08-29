f70b88e97e0fcc05ca63136e3ad08bac531eec42
Adam Robinson <adarobin@umich.edu>
2019-08-29 17:05:31 -0400

    read in individual properties of ip4 address

--- bluecat/data_source_ip4_address.go
+++ bluecat/data_source_ip4_address.go
@@ -1,8 +1,10 @@
 package bluecat
 
 import (
+	"fmt"
 	"log"
 	"strconv"
+	"strings"
 
 	"github.com/hashicorp/terraform/helper/schema"
 	"github.com/umich-vci/golang-bluecat"
@@ -16,7 +18,7 @@ func dataSourceIP4Address() *schema.Resource {
 				Type:     schema.TypeString,
 				Required: true,
 			},
-			"ip4_address": &schema.Schema{
+			"address": &schema.Schema{
 				Type:     schema.TypeString,
 				Required: true,
 			},
@@ -29,6 +31,22 @@ func dataSourceIP4Address() *schema.Resource {
 				Computed: true,
 			},
 			"type": &schema.Schema{
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"assigned_date": &schema.Schema{
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"requested_by": &schema.Schema{
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"notes": &schema.Schema{
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"state": &schema.Schema{
 				Type:     schema.TypeString,
 				Computed: true,
 			},
@@ -49,7 +67,7 @@ func dataSourceIP4AddressRead(d *schema.ResourceData, meta interface{}) error {
 		mutex.Unlock()
 		return err
 	}
-	address := d.Get("ip4_address").(string)
+	address := d.Get("address").(string)
 
 	resp, err := client.GetIP4Address(containerID, address)
 	if err = bam.LogoutClientIfError(client, err, "Failed to get IP4 Address"); err != nil {
@@ -61,6 +79,34 @@ func dataSourceIP4AddressRead(d *schema.ResourceData, meta interface{}) error {
 	d.Set("name", resp.Name)
 	d.Set("properties", resp.Properties)
 	d.Set("type", resp.Type)
+
+	props := strings.Split(*resp.Properties, "|")
+	for x := range props {
+		if len(props[x]) > 0 {
+			prop := strings.Split(props[x], "=")[0]
+			val := strings.Split(props[x], "=")[1]
+
+			switch prop {
+			case "Assigned_Date":
+				d.Set("assigned_date", val)
+			case "Requested_by":
+				d.Set("requested_by", val)
+			case "Notes":
+				d.Set("notes", val)
+			case "address":
+				// since we have to pass in an address to read it we don't really need this
+				// 	d.Set("address", val)
+			case "state":
+				d.Set("state", val)
+			default:
+				err := fmt.Errorf("Unknown IP4 Address Property: %s", prop)
+				if err = bam.LogoutClientIfError(client, err, "Unknown IP4 Address Property"); err != nil {
+					mutex.Unlock()
+					return err
+				}
+			}
+		}
+	}
 
 	// logout client
 	if err := client.Logout(); err != nil {
