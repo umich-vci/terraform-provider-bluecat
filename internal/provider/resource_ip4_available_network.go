c252a381cae4a94ac4b6a4ae1d1d78ec4a5114cd
Adam Robinson <adarobin@umich.edu>
2022-02-23 15:33:30 -0500

    allow a list of network names or ids

--- internal/provider/resource_ip4_available_network.go
+++ internal/provider/resource_ip4_available_network.go
@@ -5,6 +5,7 @@ import (
 	"hash/crc64"
 	"log"
 	"math/rand"
+	"strconv"
 	"time"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
@@ -22,13 +23,25 @@ func resourceIP4AvailableNetwork() *schema.Resource {
 
 		Schema: map[string]*schema.Schema{
 			"network_id_list": {
-				Description: "A list of Network IDs to search for a free IP address. By default, the address with the most free addresses will be returned. See the `random` argument for another selection method. The resource will be recreated if the network_id_list is changed. You may want to use a `lifecycle` customization to ignore changes to the list after resource creation so that a new network is not selected if the list is changed.",
-				Type:        schema.TypeList,
-				Required:    true,
-				ForceNew:    true,
+				Description: "A list of Network IDs to search for a free IP address. By default, the network with the most free addresses will be returned. See the `random` argument for another selection method. The resource will be recreated if the network_id_list is changed. You may want to use a `lifecycle` customization to ignore changes to the list after resource creation so that a new network is not selected if the list is changed.",
+				Type:        schema.TypeSet,
+				Optional:    true,
 				Elem: &schema.Schema{
 					Type: schema.TypeInt,
 				},
+			},
+			"network_name_list": {
+				Description: "A list of Network Names to search for a free IP address. By default, the network with the most free addresses will be returned. See the `random` argument for another selection method. The resource will be recreated if the network_id_list is changed. You may want to use a `lifecycle` customization to ignore changes to the list after resource creation so that a new network is not selected if the list is changed.",
+				Type:        schema.TypeSet,
+				Optional:    true,
+				Elem: &schema.Schema{
+					Type: schema.TypeString,
+				},
+			},
+			"container_id": {
+				Description: "The object ID of a container that contains the specified IPv4 network.",
+				Type:        schema.TypeString,
+				Optional:    true,
 			},
 			"keepers": {
 				Description: "An arbitrary map of values. If this argument is changed, then the resource will be recreated.",
@@ -67,15 +80,57 @@ func resourceIP4AvailableNetworkCreate(ctx context.Context, d *schema.ResourceDa
 
 	result := -1
 
-	networkIDList := d.Get("network_id_list").([]interface{})
-	seed := d.Get("seed").(string)
-	random := d.Get("random").(bool)
+	networkIDList := d.Get("network_id_list").(*schema.Set).List()
+	networkNameList := d.Get("network_name_list").(*schema.Set).List()
 
-	if len(networkIDList) == 0 {
-		err := gobam.LogoutClientWithError(client, "network_id_list cannot be empty")
+	if len(networkIDList) == 0 && len(networkNameList) == 0 {
+		err := gobam.LogoutClientWithError(client, "one of network_id_list or network_name_list must be set.")
 		mutex.Unlock()
 		return diag.FromErr(err)
 	}
+
+	if len(networkIDList) > 0 && len(networkNameList) > 0 {
+		err := gobam.LogoutClientWithError(client, "only one of network_id_list or network_name_list can be set.")
+		mutex.Unlock()
+		return diag.FromErr(err)
+	}
+
+	if len(networkNameList) > 0 {
+		if cid, ok := d.GetOk("container_id"); ok {
+			containerID, err := strconv.ParseInt(cid.(string), 10, 64)
+			if err = gobam.LogoutClientIfError(client, err, "Unable to convert container_id from string to int64"); err != nil {
+				mutex.Unlock()
+				return diag.FromErr(err)
+			}
+
+			for _, name := range networkNameList {
+				options := "hint=" + name.(string)
+				resp, err := client.GetIP4NetworksByHint(containerID, 0, 1, options)
+				if err = gobam.LogoutClientIfError(client, err, "Failed to get IP4 Networks by hint"); err != nil {
+					mutex.Unlock()
+					return diag.FromErr(err)
+				}
+
+				if len(resp.Item) > 1 || len(resp.Item) == 0 {
+					var diags diag.Diagnostics
+					err := gobam.LogoutClientWithError(client, "Network lookup error")
+					mutex.Unlock()
+					diags = append(diags, diag.FromErr(err)...)
+					diags = append(diags, diag.Errorf("network with name %s not found", name)...)
+					return diags
+				}
+
+				networkIDList = append(networkIDList, resp.Item[0].Id)
+			}
+		} else {
+			err := gobam.LogoutClientWithError(client, "container_id must be set when using network_name_list")
+			mutex.Unlock()
+			return diag.FromErr(err)
+		}
+	}
+
+	seed := d.Get("seed").(string)
+	random := d.Get("random").(bool)
 
 	if random {
 		rand := NewRand(seed)
